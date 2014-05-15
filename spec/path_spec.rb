b95b8222768a4b970d8c426f6de410cbe2bbbc37
Lamont Granquist <lamont@scriptkiddie.org>
2014-05-14 17:24:44 -0700

    ensure include headers get built correctly

--- spec/path_spec.rb
+++ spec/path_spec.rb
@@ -12,4 +12,10 @@ describe "when testing path helpers" do
   it "should define Libyajl2.include_path" do
     expect(Libyajl2.include_path).to include("lib/libyajl2/vendored-libyajl2/include")
   end
+
+  %w{yajl_common.h yajl_gen.h yajl_parse.h yajl_tree.h yajl_version.h}.each do |hdr|
+    it "should copy the #{hdr} header file to the gem path" do
+      expect(File.exist?(File.join(Libyajl2.include_path, "yajl", hdr))).to be_true
+    end
+  end
 end
