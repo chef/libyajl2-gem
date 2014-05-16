f084836e0790c081dedb7c7886a04ee26f5b1732
Lamont Granquist <lamont@scriptkiddie.org>
2014-05-16 14:08:01 -0700

    remove POSTLINK entirely

--- ext/libyajl2/extconf.rb
+++ ext/libyajl2/extconf.rb
@@ -156,7 +156,6 @@ OBJS = yajl.o yajl_alloc.o yajl_buf.o yajl_encode.o yajl_gen.o yajl_lex.o yajl_p
 HDRS = yajl_alloc.h yajl_buf.h yajl_bytestack.h yajl_encode.h yajl_lex.h yajl_parser.h
 TARGET = libyajl
 DLLIB = $(TARGET).#{config['DLEXT']}
-POSTLINK = #{config['POSTLINK']}
 
 TIMESTAMP_DIR = .
 RUBYARCHDIR   = $(sitearchdir)$(target_prefix)
@@ -190,7 +189,6 @@ $(DLLIB): $(OBJS) Makefile
 \t$(ECHO) linking shared-object $(DLLIB)
 \t-$(Q)$(RM) $(@)
 \t$(Q) $(LDSHARED) -o $@ $(OBJS) $(LIBPATH) $(DLDFLAGS) $(LIBS)
-\t$(Q) $(POSTLINK)
 
 $(OBJS): $(HDRS)
 EOF
