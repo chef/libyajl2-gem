dd0324f806531c0ff31f55d9bb70804ba19677e2
Stan Hu <stanhu@gmail.com>
2023-06-12 15:48:46 -0700

    fix: rename Windows implib to use .lib extension instead of .a
    By default, omnibus-software will clean up all *.a files in the Ruby
    gem path
    (https://github.com/chef/omnibus-software/blob/e43f50af2e03dc073174f8de819d08432b9b22b7/config/software/ruby-cleanup.rb#L40-L53).
    
    By convention, Windows implib files should have a .lib extension
    (https://learn.microsoft.com/en-us/cpp/build/reference/implib-name-import-library?view=msvc-170),
    so this change makes it possible to preserve the required library.
    
    Relates to https://github.com/chef/ffi-yajl/pull/114
    
    Signed-off-by: Stan Hu <stanhu@gmail.com>

--- ext/libyajl2/extconf.rb
+++ ext/libyajl2/extconf.rb
@@ -52,7 +52,7 @@ module Libyajl2Build
 
     # create the implib on windows
     if windows?
-      $LDFLAGS << " -Wl,--export-all-symbols -Wl,--enable-auto-import -Wl,--out-implib=libyajldll.a -Wl,--output-def,libyajl.def"
+      $LDFLAGS << " -Wl,--export-all-symbols -Wl,--enable-auto-import -Wl,--out-implib=libyajldll.lib -Wl,--output-def,libyajl.def"
     end
 
     $CFLAGS << " -DNDEBUG"
@@ -117,7 +117,7 @@ EOF
 install:
 \tmkdir -p #{prefix}/lib
 \tcp libyajl.so #{prefix}/lib/libyajl.so
-\tcp libyajldll.a #{prefix}/lib/libyajldll.a
+\tcp libyajldll.lib #{prefix}/lib/libyajldll.lib
 \tcp libyajl.def #{prefix}/lib/libyajl.def
 \tmkdir -p #{prefix}/include/yajl
 \tcp yajl/*.h #{prefix}/include/yajl
